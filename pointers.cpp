a0d54e0d74972ac0dabb15f95eaae0f5ad7fc8ac
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-15 21:19:50 +0600

    pointer

---
+++ pointers.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int var = 10;
+    
+    // Store the address of 
+    // var variable
+    int* ptr = &var;
+    
+    // Access the address value
+    cout << ptr;
+    return 0;
+}
