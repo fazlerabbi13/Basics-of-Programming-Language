70c56499f21ab815b967e9b53d75f1daac1005fb
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-15 21:27:42 +0600

    The pointer can be modified to point to another memory address if its is of the same type.

---
+++ modifyAddress.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 99;
+
+    int *ptr = &a;
+    cout << *ptr << endl;
+    
+    // Changing the address stored
+    ptr = &b;
+    cout << *ptr;
+    
+    return 0;
+}
